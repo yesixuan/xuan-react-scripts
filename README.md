3130e80485a48588233bdfba7b9904c05f91e48a
yesixuan <18373209286@163.com>
2018-10-28 11:26:59 +0800

    init: 初始化 react-scripts

---
+++ README.md
@@ -0,0 +1,4 @@
+# xuan-react-scripts
+
+自己定制的 react-scripts   
+兼容 create-react-app 2.0.4 版本
---
+++ template/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>私人订制 react-scripts</h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
